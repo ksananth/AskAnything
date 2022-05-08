2b36d6bd219825858421620d7ac108b594975ca2
Ananth <anan.busyguy007@gmail.com>
2022-05-08 07:22:11 +0200

    Add login page to MainActivity.kt

--- app/src/main/java/com/kraftanapp/askanything/MainActivity.kt
+++ app/src/main/java/com/kraftanapp/askanything/MainActivity.kt
@@ -15,7 +15,9 @@ import com.kraftanapp.askanything.ui.theme.AskAnythingTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+
             AskAnythingTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
